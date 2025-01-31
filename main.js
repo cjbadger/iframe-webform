c868b5fef687d7189adf110043e990d2ed8d8017
cjbadger <94421801+cjbadger@users.noreply.github.com>
2025-01-31 13:52:37 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("<<< >>>")
+const getQueryParam = (name) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const iframe = document.getElementById("entity-form");
+  const urlParam = getQueryParam("url");
+  if (iframe && urlParam) {
+    iframe.src = decodeURIComponent(urlParam);
+  } else {
+    console.log("Append the URL to be used as the iframe's source as a query parameter with the key 'url'; eg cjbadger.github.io/iframe-webform?url=https://example.com
+  }
+});
