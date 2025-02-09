93f0f9224ba9daf76670dc67ae5d55fc86dc012b
cjbadger <94421801+cjbadger@users.noreply.github.com>
2025-02-10 07:58:01 +1100

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Append the URL to be used as the iframe's source as a query parameter with the key 'url'; eg cjbadger.github.io/iframe-webform?url=https://example.com");
   }
 });
+
+  window.addEventListener("message", function(event) {
+    console.log(`event received`);
+    console.log(`event.origin: ${event.origin}`);
+    console.log(`event.data.iframeHeight: ${event.data?.iframeHeight}`);
+    if (event.origin !== "https://site-q89tn.powerappsportals.com") {
+      return;
+    }
+
+    const iframe = document.getElementById("enquiry-form");
+    if (event.data?.iframeHeight) {
+      iframe.style.height = event.data?.iframeHeight + "px";
+    }
+  });
