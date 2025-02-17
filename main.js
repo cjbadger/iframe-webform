ee622219220f7d17d1bbfa9b9b5f852646e4edbe
cjbadger <94421801+cjbadger@users.noreply.github.com>
2025-02-17 21:22:32 +1100

    Update main.js

--- main.js
+++ main.js
@@ -23,9 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const EnquiryFormResizeHandler = () => {
   const trustedOrigins = [
-    "https://site-q89tn.powerappsportals.com", // Dev
-    "https://site-jtiyc.powerappsportals.com", // UAT
-    "https://mlc-enquiry-form.powerappsportals.com" // Prod
+    "https://site-tt3fl.powerappsportals.com", // WLSQ UAT
+    "https://site-q89tn.powerappsportals.com", // MLC Dev
+    "https://site-jtiyc.powerappsportals.com", // MLC UAT
+    "https://mlc-enquiry-form.powerappsportals.com" // MLC Prod
   ];
   
   window.addEventListener("message", (event) => {
